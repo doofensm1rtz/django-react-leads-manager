7ada870f1ac181fda78733de80989cc6de71b904
doofensm1rtz <cocsuyash01@gmail.com>
2020-09-26 18:35:03 +0530

    Header and basic dashboard

---
+++ leadsmanager/frontend/src/components/leads/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Form extends Component {
+  render() {
+    return (
+      <>
+        <h2>Form</h2>
+      </>
+    );
+  }
+}
+
+export default Form;
---
+++ leadsmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Leads extends Component {
+  render() {
+    return (
+      <>
+        <h2>Leads</h2>
+      </>
+    );
+  }
+}
+
+export default Leads;
